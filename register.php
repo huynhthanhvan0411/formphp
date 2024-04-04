f6b2314a59d275aadb4c59a9f2c8cc4b2c35f6e4
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-04 15:07:51 +0700

    lan 2

--- register.php
+++ register.php
@@ -1,10 +1,19 @@
 <?php
 // Khai báo biến để lưu thông báo lỗi
 $errors = [];
+//define  
+$name = "";
+$age = "";
+$address = "";
+$phone = "";
+$employment = "";
+$email = "";
+$password = "";
+$submitted = false;
 
 
 // Kiểm tra xem có dữ liệu được gửi đi không
-if (isset($_REQUEST['name']) && isset($_REQUEST['age']) && isset($_REQUEST['address']) && isset($_REQUEST['phone']) && isset($_REQUEST['employment']) && isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Lấy giá trị từ form
     $name = $_REQUEST['name'];
     $age = $_REQUEST['age'];
@@ -58,8 +67,8 @@ if (isset($_REQUEST['name']) && isset($_REQUEST['age']) && isset($_REQUEST['addr
 </head>
 
 <body>
-<h1>Đăng kí tài khoản</h1>
-<?php if (isset($submitted) && $submitted) : ?>
+    <h1>Đăng kí tài khoản</h1>
+    <?php if (isset($submitted) && $submitted) : ?>
     <h2>Thông tin đã nhập:</h2>
     <p><strong>Name:</strong> <?php echo $name; ?></p>
     <p><strong>Age:</strong> <?php echo $age; ?></p>
@@ -68,44 +77,45 @@ if (isset($_REQUEST['name']) && isset($_REQUEST['age']) && isset($_REQUEST['addr
     <p><strong>Employment:</strong> <?php echo $employment; ?></p>
     <p><strong>Email:</strong> <?php echo $email; ?></p>
     <p><strong>Password:</strong> ***</p>
-<?php else : ?>
+    <?php else : ?>
     <form method="post" action="">
         <div class="mb-3">
             <label for="name">Name</label><br>
-            <input type="text" name="name" id="name"  required>
+            <input type="text" name="name" id="name" required value="<?php echo $name; ?>">
             <?php if (isset($errors['name'])) echo "<span style='color: red;'>" . $errors['name'] . "</span>"; ?>
         </div>
         <div class="mb-3">
             <label for="age">Age</label><br>
-            <input type="number" name="age" id="age" >
+            <input type="number" name="age" id="age" value="<?php echo $age; ?>">
         </div>
         <div class="mb-3">
             <label for="address">Address</label><br>
-            <input type="text" autocomplete="off" name="address" id="address" >
+            <input type="text" autocomplete="off" name="address" id="address" value="<?php echo $address; ?>">
         </div>
         <div class="mb-3">
             <label for="phone">Phone</label><br>
-            <input type="tel" name="phone" id="phone"  required>
+            <input type="tel" name="phone" id="phone" required value="<?php echo $phone; ?>">
             <?php if (isset($errors['phone'])) echo "<span style='color: red;'>" . $errors['phone'] . "</span>"; ?>
         </div>
         <div class="mb-3">
             <label for="employment">Employment</label><br>
-            <input type="text" name="employment" id="employment" required>
+            <input type="text" name="employment" id="employment" required value="<?php echo $employment; ?>">
         </div>
         <div class="mb-3">
             <label for="email">Email</label><br>
-            <input type="email" name="email" id="email" autocomplete="off" required>
+            <input type="email" name="email" id="email" autocomplete="off" required value="<?php echo $email; ?>">
             <?php if (isset($errors['email'])) echo "<span style='color: red;'>" . $errors['email'] . "</span>"; ?>
         </div>
         <div class="mb-3">
             <label for="password">Password</label><br>
-            <input type="password" name="password" id="password" autocomplete="off" required>
+            <input type="password" name="password" id="password" autocomplete="off" required
+                value="<?php echo $password; ?>">
             <?php if (isset($errors['password'])) echo "<span style='color: red;'>" . $errors['password'] . "</span>"; ?>
         </div>
 
         <button type="submit">Submit</button>
     </form>
-<?php endif; ?>
+    <?php endif; ?>
 </body>
 
 </html>
--- validation.php
+++ validation.php
@@ -1,7 +1,11 @@
 <?php
 $errors=[];
-//check da ton tai chua
-if(isset($_REQUEST['username']) && isset($_REQUEST['phone']) && isset($_REQUEST['email'])) {
+//check da ton tai chua]
+//define 
+$username = "";
+$phone = "";
+$email = "";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_REQUEST['username'] ;
     $phone = $_REQUEST['phone'];
     $email = $_REQUEST['email'];
@@ -10,11 +14,11 @@ if(isset($_REQUEST['username']) && isset($_REQUEST['phone']) && isset($_REQUEST[
 //    if(empty($username) || empty($phone) || empty($email)) {
 //       $errors['empty'] = "Vui long nhap thong tin";
 //    }
-    if(!preg_match("/^[a-zA-Z]{6,10}$/", $username) ) {
-       $errors['username'] = "Phải có ít nhất 1 chữ cái hoa và thường và ít nhất 6 kí tự";
+    if(!preg_match("/^[a-zA-Z0-9]{6,20}$/", $username) ) {
+       $errors['username'] = "Phải có ít nhất 1 chữ cái hoa và thường và ít nhất 6 kí tự và nhiều nhất 20 kí tự";
     }
     if(!preg_match("/^(038|035)[0-9]{5,7}$/", $phone)){
-        $errors['phone'] = "Dinh dang khong hop le";
+        $errors['phone'] = "Bắt đầu là 038 hoặc 035 và phải có ít nhất 8 kí tự số";
     }
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $errors['email'] = "Dinh dang khong hop le";
@@ -28,39 +32,42 @@ if(isset($_REQUEST['username']) && isset($_REQUEST['phone']) && isset($_REQUEST[
 }
 ?>
 <html>
+
 <head>
     <title>Validation</title>
 </head>
+
 <body>
-<?php if (isset($submitted) && $submitted) :
+    <?php if (isset($submitted) && $submitted) :
     echo "Username:". $username ."<br>";
     echo "Phone". $phone ."<br>";
     echo "Email:".$email ."<br>";
     ?>
 
-<?php else : ?>
-<form method="post" action="validation.php">
-    <div class="mt-3">
-        <label for="username" class="form-label">Username</label>
-        <br>
-        <input type="text" class="form-control" id="username" name="username" required>
-        <?php if (isset($errors['username'])) echo "<span style='color: red;'>" . $errors['username'] . "</span>"; ?>
-    </div>
-    <div class="mt-3">
-        <label for="phone" class="form-label">Phone</label>
-        <br>
-        <input type="tel" class="form-control" id="phone" name="phone" required>
-        <?php if (isset($errors['phone'])) echo "<span style='color: red;'>" . $errors['phone'] . "</span>"; ?>
-    </div>
-    <div class="mt-3">
-        <label for="email" class="form-label">Email</label>
-        <br>
-        <input type="email" class="form-control" id="email" name="email" required>
-        <?php if (isset($errors['email'])) echo "<span style='color: red;'>" . $errors['email'] . "</span>"; ?>
-    </div>
-    <input type="submit" value="submit">
-</form>
-<?php endif; ?>
+    <?php else : ?>
+    <form method="post" action="validation.php">
+        <div class="mt-3">
+            <label for="username" class="form-label">Username</label>
+            <br>
+            <input type="text" class="form-control" id="username" name="username" required
+                value="<?php echo $username; ?>">
+            <?php if (isset($errors['username'])) echo "<span style='color: red;'>" . $errors['username'] . "</span>"; ?>
+        </div>
+        <div class="mt-3">
+            <label for="phone" class="form-label">Phone</label>
+            <br>
+            <input type="tel" class="form-control" id="phone" name="phone" required value="<?php echo $phone; ?>">
+            <?php if (isset($errors['phone'])) echo "<span style='color: red;'>" . $errors['phone'] . "</span>"; ?>
+        </div>
+        <div class="mt-3">
+            <label for="email" class="form-label">Email</label>
+            <br>
+            <input type="email" class="form-control" id="email" name="email" required value="<?php echo $email; ?>">
+            <?php if (isset($errors['email'])) echo "<span style='color: red;'>" . $errors['email'] . "</span>"; ?>
+        </div>
+        <input type="submit" value="submit">
+    </form>
+    <?php endif; ?>
 </body>
-</html>
 
+</html>
